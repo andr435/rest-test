d4e85fcac7c9df69bceb366ef8b71744bedbc99a
Andrey Mussatov <andr435@gmail.com>
2019-03-31 16:52:06 +0300

    Update ApiuserProvider.php

--- src/Security/User/ApiuserProvider.php
+++ src/Security/User/ApiuserProvider.php
@@ -7,6 +7,7 @@ use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use App\Repository\UserRepository;
 
 
 class ApiuserProvider implements UserProviderInterface
